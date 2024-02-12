68ee288078cfd504b84e911af4197a61f84d7d31
Sayan Mallick <lemon@Sayans-MacBook-Pro.local>
2024-02-12 10:50:35 +0530

    docker

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Elysia } from "elysia";
+import { logger } from "@bogeychan/elysia-logger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(
+		logger({
+			autoLogging: true,
+		}),
+	)
+	.get("/", () => "Hello Elysia")
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
